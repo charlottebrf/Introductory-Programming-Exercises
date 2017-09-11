21464c6e75071868abe3cce7bb6360cdb85619c7
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-11 11:13:05 +0100

    Adds example test files & initial Test file for first exercise

--- tests/DrawHorizontalLineTest.java
+++ tests/DrawHorizontalLineTest.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+package junitfaq;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import java.util.*;
 
 public class DrawHorizontalLineTest {
+
+
 }
+
+
--- tests/ExampleTests.java
+++ tests/ExampleTests.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
-public class ExampleTests {
-}
+//public class ExampleTests {
+//
+//    @Test
+//    public void firstTest() {
+//        Assert.assertTrue(true);
+//    }
+//
+//    @Test
+//    public void printOneAstericksWhenGivenZero() throws Exception {
+//        assertEquals(0, getNumberAtPositionInSequence(0));
+//    }
+//
+////    @Test
+////    public void printOneAstericksWhenGivenOne() throws Exception {
+////        assertEquals(0, getNumberAtPositionInSequence(1));
+////    }
+//
+//    //helper method for duplicated code
+////    private int getNumberAtPositionInSequence(int position) {
+////        return new HorizontalLine().getNumberAtPosition(position);
+////    }
+//
+//
+//    @Test
+//    public void name() throws Exception {
+//    }
+//
+//    {
+//    }
