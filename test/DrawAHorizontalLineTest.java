0dd498153dc035e07e7c23423dd03ad27ed94055
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-11 14:30:06 +0100

    Draws a horizontal line exercise:
    - Refactors tests to use integers
    - Adds in tests for 0 & 25

--- test/DrawAHorizontalLineTest.java
+++ test/DrawAHorizontalLineTest.java
@@ -5,16 +5,26 @@ import static org.junit.Assert.*;
 public class DrawAHorizontalLineTest {
 
     @Test
+    public void printsAnEmptyString() {
+        DrawAHorizontalLine line = new DrawAHorizontalLine();
+        assertEquals("prints an empty string when given 0", "", line.printer(0));
+    }
+
+    @Test
     public void printsOneAsterisk() {
         DrawAHorizontalLine line = new DrawAHorizontalLine();
-        Integer one = 1;
-        assertEquals("prints an asterisk when given 1", "*", line.printer(one));
+        assertEquals("prints an asterisk when given 1", "*", line.printer(1));
     }
 
     @Test
     public void printsTwoAsterisks() {
         DrawAHorizontalLine line = new DrawAHorizontalLine();
-        Integer two = 2;
-        assertEquals("prints two asterisks when given 2", "**", line.printer(two));
+        assertEquals("prints two asterisks when given 2", "**", line.printer(2));
+    }
+
+    @Test
+    public void printsTenAsterisks() {
+        DrawAHorizontalLine line = new DrawAHorizontalLine();
+        assertEquals("prints ten asterisks when given 10", "**********", line.printer(10));
     }
 }
