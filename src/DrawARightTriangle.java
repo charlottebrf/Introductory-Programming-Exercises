5c83c614f50813cd64c776664018fd09bce5ae87
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-11 18:36:25 +0100

    Refactors solution

--- src/DrawARightTriangle.java
+++ src/DrawARightTriangle.java
@@ -2,9 +2,9 @@ public class DrawARightTriangle {
 
     String printer(int num) {
         String asterisk = "*";
-        String numberAsAsterisk = null;
+        String numberAsAsterisk = new String();
         for (int i = 0; i < num; i++) {
-            numberAsAsterisk = asterisk;
+             numberAsAsterisk = new String(new char[i]).replace("\0", asterisk);
         }
         return numberAsAsterisk + "\n";
     }
