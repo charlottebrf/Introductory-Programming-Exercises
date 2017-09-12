ddcac5b0ee4c6d8e0e710eb26415961f376fbbdb
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-12 19:26:52 +0100

    Refactors the tests

--- test/DiamondTest.java
+++ test/DiamondTest.java
@@ -20,23 +20,21 @@ public class DiamondTest {
     @Test
     public void returnstheRestOfTheTriangle() {
         Diamond diamond = new Diamond();
-        IsoscelesTriangle isoscelesTriangle = new IsoscelesTriangle();
-        String result1 = isoscelesTriangle.asteriskGenerator(1);
-        result1 = isoscelesTriangle.formatter(result1);
-        String result2 = isoscelesTriangle.asteriskGenerator(3);
-        result2 = isoscelesTriangle.formatter(result2);
+        String result1 = diamond.asteriskGenerator(1);
+        result1 = diamond.formatter(result1);
+        String result2 = diamond.asteriskGenerator(3);
+        result2 = diamond.formatter(result2);
 
-        assertEquals("prints an Isosceles of 1,3 & 5 asterisks when given 3", result2 + "\n" + result1 + "\n", diamond.upsideDownIsosceles(3));
+        assertEquals("prints an upside down Isosceles of 3,1 when given 3", result2 + "\n" + result1 + "\n", diamond.upsideDownIsosceles(3));
     }
 
     @Test
     public void returnsADiamond() {
         Diamond diamond = new Diamond();
-        IsoscelesTriangle isoscelesTriangle = new IsoscelesTriangle();
-        String result1 = isoscelesTriangle.asteriskGenerator(1);
-        result1 = isoscelesTriangle.formatter(result1);
-        String result2 = isoscelesTriangle.asteriskGenerator(3);
-        result2 = isoscelesTriangle.formatter(result2);
+        String result1 = diamond.asteriskGenerator(1);
+        result1 = diamond.formatter(result1);
+        String result2 = diamond.asteriskGenerator(3);
+        result2 = diamond.formatter(result2);
         String result3 = diamond.asteriskGenerator(5);
         result3 = diamond.formatter(result3);
 
