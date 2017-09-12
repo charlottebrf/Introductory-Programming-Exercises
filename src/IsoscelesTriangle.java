044b47a6cc88873b4a5db09cd9683fa447757e43
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-12 11:43:15 +0100

    Adds in extra methods to build alorithm:
    - Subtracts 2 from an integer
    - Checks if the integer is greater than one- if it is it will invoke subtractTwo

--- src/IsoscelesTriangle.java
+++ src/IsoscelesTriangle.java
@@ -1,5 +1,23 @@
 import org.apache.commons.lang3.StringUtils;
+import java.util.*;
+
 public class IsoscelesTriangle {
+
+    public int subtractTwo(int num) {
+        return num - 2;
+    }
+
+    public int GreaterThanOne(int num) {
+        int result = 0;
+        if (num > 1) {
+            result += this.subtractTwo(num);
+        } else if (num == 1) {
+            result += 1;
+        } else {
+            result += 0;
+        }
+        return result;
+    }
 
     public String printer(int num) {
         String result = "";
@@ -11,3 +29,16 @@ public class IsoscelesTriangle {
     }
 
 }
+
+
+
+//Problem 1: isosceles
+//
+//        For num = 3;
+//
+//        1, 3, 5
+//
+//        Add the result of each to some kind of a list so they are in correct order and then print centred:
+//        Method 1: Print asterisks for num - 2 until you get to 1;
+//        Method 2: Print asterisks for num;
+//        Print asterisks for num + 2;
--- test/IsoscelesTriangleTest.java
+++ test/IsoscelesTriangleTest.java
@@ -6,16 +6,55 @@ import static org.junit.Assert.*;
 public class IsoscelesTriangleTest {
 
     @Test
+    public void SubtractsTwoFromANumber() {
+        IsoscelesTriangle isoscelesTriangle = new IsoscelesTriangle();
+        assertEquals("subracts 2 from a given number", 1, isoscelesTriangle.subtractTwo(3));
+    }
+
+    @Test
+    public void SubtractsTwoFromAnotherNumber() {
+        IsoscelesTriangle isoscelesTriangle = new IsoscelesTriangle();
+        assertEquals("subracts 2 from a given number", 3, isoscelesTriangle.subtractTwo(5));
+    }
+
+    @Test
+    public void SubtractsTwoFromANumberIfGreaterThanOne() {
+        IsoscelesTriangle isoscelesTriangle = new IsoscelesTriangle();
+        assertEquals("subtracts two if number is greater than one", 1, isoscelesTriangle.GreaterThanOne(3));
+    }
+
+    @Test
+    public void IfOneReturnsOne() {
+        IsoscelesTriangle isoscelesTriangle = new IsoscelesTriangle();
+        assertEquals("returns one if number is one", 1, isoscelesTriangle.GreaterThanOne(1));
+    }
+
+    //This could be a good test to practice exceptions on
+    @Test
+    public void IfZeroReturnsZero() {
+        IsoscelesTriangle isoscelesTriangle = new IsoscelesTriangle();
+        assertEquals("returns zero if number is zero or less", 0, isoscelesTriangle.GreaterThanOne(0));
+    }
+
+    @Test
+    public void IfLessThanZeroReturnsZero() {
+        IsoscelesTriangle isoscelesTriangle = new IsoscelesTriangle();
+        assertEquals("returns zero if number is zero or less", 0, isoscelesTriangle.GreaterThanOne(-7));
+    }
+
+    @Test
     public void CentresAsteriskFor1() {
         IsoscelesTriangle isoscelesTriangle = new IsoscelesTriangle();
         String result1 = StringUtils.center(isoscelesTriangle.printer(1), 10);
         assertEquals("prints a centred asterisk when given 1",result1, isoscelesTriangle.printer(1));
     }
 
-    @Test
-    public void CentresAsteriskFor2() {
-        IsoscelesTriangle isoscelesTriangle = new IsoscelesTriangle();
-        String result1 = StringUtils.center(isoscelesTriangle.printer(2), 10);
-        assertEquals("prints a centred asterisk when given 2", result1 , isoscelesTriangle.printer(2));
-    }
+
+
+    //    @Test
+//    public void CentresAsteriskFor2() {
+//        IsoscelesTriangle isoscelesTriangle = new IsoscelesTriangle();
+//        String result1 = StringUtils.center(isoscelesTriangle.printer(2), 10);
+//        assertEquals("prints a centred asterisk when given 2", result1 , isoscelesTriangle.printer(2));
+//    }
 }
