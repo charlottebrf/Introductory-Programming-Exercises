e8c740596937a678a06512cbe2e096dfb86a9192
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-13 12:38:14 +0100

    Updates with current solution, however doesn't yet work for 30 or sort

--- src/primeNumbers.java
+++ src/primeNumbers.java
@@ -1,44 +1,25 @@
 import java.lang.reflect.Array;
+import java.lang.*;
 import java.util.*;
 
 public class primeNumbers {
 
     public ArrayList<Integer> generate(Integer num) {
-        ArrayList<Integer> list = new ArrayList<Integer>(0);
-        return this.isOneOrSelf(list, num);
+        return this.primeNumberGenerator(num);
     }
 
-    public ArrayList<Integer> isOneOrSelf(ArrayList<Integer> list, Integer num) {
-        ArrayList<Integer> numList = new ArrayList<Integer>(num);
-        if (list == numList || num == 1) {
-            return list;
+    public ArrayList<Integer> primeNumberGenerator(Integer num) {
+        ArrayList<Integer> result = new ArrayList<Integer>(0);
+        Integer temp = 0;
+
+
+        for (Integer i = 2; i <= num/2; i++) {
+            temp = num % i;
+            if(temp == 0 && i / 1 == i && i / i == 1 && (i / 2 == 1 || (i / 2) != Math.floor(i / 2))) {
+                result.add(i);
+            }
         }
-        return list;
+        return result;
     }
 
-
-    //must be a positive integer greater than 1
-
-
-    //when a prime number is divided by another positive integer, the result will only be a positive integer when the prime number is divided by itself & the number 1
-
-//    IS NOT PRIME
-//    4 % 2 = 2 - still positive
-//    6 / 3 = 2
-//            6 / 2 = 3 ;
-//    not prime numbers
-//
-//    IS PRIME: only divided by self & number 1 to produce positive integers
-//    2 / 2 = 1
-//    2 / 1 = 2
-
-
-//    3 / 3 = 1;
-//    3 / 1 = 3;
-
-
-//    Can't be itself or 1
-
-
-//    find prime numbers for that number from 1 to number; e.g. up to 30 in the case of 30
 }
--- test/primeNumbersTest.java
+++ test/primeNumbersTest.java
@@ -1,5 +1,5 @@
 import org.junit.Test;
-
+import java.lang.*;
 import java.util.*;
 
 import static org.junit.Assert.*;
@@ -10,13 +10,34 @@ public class primeNumbersTest {
     public void returnsPrimeofOne() {
         primeNumbers prime = new primeNumbers();
         ArrayList<Integer> list = new ArrayList<Integer>(0);
-        assertEquals("returns only prime for 1", list , prime.generate(1));
+        assertEquals("1 is not a prime number so returns empty", list , prime.generate(1));
     }
 
     @Test
     public void returnsPrimeofTwo() {
         primeNumbers prime = new primeNumbers();
         ArrayList<Integer> list = new ArrayList<Integer>(0);
-        assertEquals("returns only prime for 1", list , prime.generate(2));
+        assertEquals("although 2 is a prime number algorithm won't return itself or 1", list , prime.generate(2));
     }
+
+    @Test
+    public void returnsPrimeofTwelve() {
+        primeNumbers prime = new primeNumbers();
+        ArrayList<Integer> list = new ArrayList<Integer>(0);
+        list.add(2);
+        list.add(3);
+        assertEquals("2 & 3 are factors of 12 & prime numbers", list , prime.generate(12));
+    }
+
+
+    @Test
+    public void returnsPrimeofThirty() {
+        primeNumbers prime = new primeNumbers();
+        ArrayList<Integer> list = new ArrayList<Integer>(0);
+        list.add(2);
+        list.add(3);
+        list.add(5);
+        assertEquals("2, 3 & 5 are factors of 30 & prime numbers", list , prime.generate(30));
+    }
+
 }
