13c885782c05e1fdb451e986c47d3c4891c4cc64
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-13 09:39:56 +0100

    If one is given, an empty ArrayList is returned

---
+++ src/primeNumbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class primeNumbers {
+
+    public ArrayList<Integer> generate(Integer num) {
+        ArrayList<Integer> list = new ArrayList<Integer>(0);
+        if (num == 1) {
+            return list;
+        }
+        return list;
+    }
+}
---
+++ test/primeNumbersTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class primeNumbersTest {
+
+    @Test
+    public void returnsPrimeofOne() {
+        primeNumbers prime = new primeNumbers();
+        ArrayList<Integer> list = new ArrayList<Integer>(0);
+        assertEquals("returns only prime for 1", list , prime.generate(1));
+    }
+}
